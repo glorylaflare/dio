d1d22f5e7d1aab9ab00574d018b07dd3ef9e66f5
Marcelo <48776043+glorylaflare@users.noreply.github.com>
2023-01-05 15:40:40 -0300

    Add arquivos da Pokedex

---
+++ Pokedex/assets/js/index.js
@@ -0,0 +1,44 @@
+// Shift + Alt + F ~> organiza arquivo
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+
+const limitMax = 151;
+const limit = 5;
+let offset = 0;
+
+// limite 248
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
+            </div>
+        </li>
+        `).join('')
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+
+    const qtdRecord = offset + limit
+
+    if (qtdRecord >= limitMax) {
+        const newLimit = limitMax - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+})
