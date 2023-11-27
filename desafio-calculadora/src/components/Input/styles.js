1747861fe772374106a0f8ae1fabe0dba2ebc565
Marcelo Gonçalves <48776043+glorylaflare@users.noreply.github.com>
2023-11-27 14:39:06 -0300

    add desafio-calculadora

---
+++ desafio-calculadora/src/components/Input/styles.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAAAFF;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+    
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #AAAAFF;
+        border: none;
+        text-align: right;
+        
+        font-size: 24px;
+        font-family: 'Roboto';
+        padding: 0 10px;
+
+        color: #FFFFFF;
+    }
+`
